bce63ffdaab123ac88bfeedf5cd0c0f892d9cb7b
J. Ryan Stinnett <jryans@gmail.com>
2018-10-08 00:39:24 +0200

    Improve wrapper for non-KLEE builds

--- verification/klee/include/klee.h
+++ verification/klee/include/klee.h
@@ -3,14 +3,14 @@
  * with no effect.
  */
 
+#ifdef KLEE_VERIFICATION
 // Define this to avoid redefintion errors via `inttypes.h` in `klee/klee.h`
 #define _INTTYPES_H
 #include <klee/klee.h>
-
-#ifndef KLEE_VERIFICATION
+#else // KLEE_VERIFICATION
 void klee_make_symbolic(void *addr, size_t nbytes, const char *name) {}
 int klee_range(int begin, int end, const char *name) {
   return begin;
 }
 #define klee_assert(expr)
-#endif // !KLEE_VERIFICATION
+#endif // KLEE_VERIFICATION
