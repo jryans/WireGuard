a935b1e3f988adf5b8acab8040c23623e2b3bf4f
J. Ryan Stinnett <jryans@gmail.com>
2018-10-10 21:38:09 +0200

    Add more stubs for mutex

--- verification/stubs/linux/mutex.c
+++ verification/stubs/linux/mutex.c
@@ -1,5 +1,10 @@
 #include <linux/mutex.h>
 
+void __mutex_init(struct mutex *lock, const char *name,
+                  struct lock_class_key *key)
+{
+}
+
 void mutex_lock(struct mutex *lock)
 {
 }
