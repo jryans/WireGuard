1864155f5158a0402db2e8eea2f0bc71f7b8e5be
J. Ryan Stinnett <jryans@gmail.com>
2018-10-10 22:04:34 +0200

    Add allocation of `net_device` to harness
    This allows KLEE to progress further down paths that interact with data
    structures related to the `net_device`.  For example, there are some
    paths that recover the `wg_device` from the `net_device`, so it seems
    easiest to create a stub of this as well.

--- verification/harness/vh_main.c
+++ verification/harness/vh_main.c
@@ -4,19 +4,116 @@ extern void *malloc(size_t __size);
 
 #include <linux/module.h>
 #include <linux/skbuff.h>
-#include <net/udp_tunnel.h>
 #include <net/inet_sock.h>
+#include <net/pkt_sched.h>
+#include <net/udp_tunnel.h>
 
 #include <kernel_config.h>
 #include <klee.h>
 
-#include "../../device.h"
-#include "../../socket.h"
+#include <device.h>
+#include <socket.h>
 
+extern struct rtnl_link_ops wg_link_ops;
 extern struct inet_sock sock_ipv4_0;
 extern udp_tunnel_encap_rcv_t sock_ipv4_0_receive;
 
-struct wg_device wg0;
+/**
+ * Initialize `net_device` similar to `alloc_netdev_mqs`.
+ */
+struct net_device* stub_alloc_netdev_mqs() {
+  unsigned int txqs = 1;
+  unsigned int rxqs = 1;
+
+  size_t device_size = sizeof(struct net_device);
+  /* ensure 32-byte alignment of private area */
+  device_size = ALIGN(device_size, NETDEV_ALIGN);
+  device_size += sizeof(struct wg_device);
+  /* ensure 32-byte alignment of whole construct */
+  device_size += NETDEV_ALIGN - 1;
+
+  struct net_device *dev = malloc(device_size);
+  dev->padded = 0;
+
+  // dev->pcpu_refcnt = alloc_percpu(int);
+  // dev_addr_init(dev);
+  // dev_mc_init(dev);
+  // dev_uc_init(dev);
+  // dev_net_set(dev, &init_net);
+  dev->gso_max_size = GSO_MAX_SIZE;
+  dev->gso_max_segs = GSO_MAX_SEGS;
+  INIT_LIST_HEAD(&dev->napi_list);
+  INIT_LIST_HEAD(&dev->unreg_list);
+  INIT_LIST_HEAD(&dev->close_list);
+  INIT_LIST_HEAD(&dev->link_watch_list);
+  INIT_LIST_HEAD(&dev->adj_list.upper);
+  INIT_LIST_HEAD(&dev->adj_list.lower);
+  INIT_LIST_HEAD(&dev->ptype_all);
+  INIT_LIST_HEAD(&dev->ptype_specific);
+#ifdef CONFIG_NET_SCHED
+  hash_init(dev->qdisc_hash);
+#endif
+  dev->priv_flags = IFF_XMIT_DST_RELEASE | IFF_XMIT_DST_RELEASE_PERM;
+
+  // Call WG `setup` op for the new device.
+  // Ops recorded as global by stubs in `rtnetlink.c`
+  wg_link_ops.setup(dev);
+
+  if (!dev->tx_queue_len) {
+    dev->priv_flags |= IFF_NO_QUEUE;
+    dev->tx_queue_len = DEFAULT_TX_QUEUE_LEN;
+  }
+
+  dev->num_tx_queues = txqs;
+  dev->real_num_tx_queues = txqs;
+  // netif_alloc_netdev_queues(dev);
+
+#ifdef CONFIG_SYSFS
+  dev->num_rx_queues = rxqs;
+  dev->real_num_rx_queues = rxqs;
+  // netif_alloc_rx_queues(dev);
+#endif
+
+  strcpy(dev->name, "wg0");
+  dev->name_assign_type = NET_NAME_USER;
+  dev->group = INIT_NETDEV_GROUP;
+  // if (!dev->ethtool_ops) {
+  //   dev->ethtool_ops = &default_ethtool_ops;
+  // }
+
+  // nf_hook_ingress_init(dev);
+
+  return dev;
+}
+
+/**
+ * Create a new link similiar to `rtnl_newlink`:
+ *
+ *   - Loads the kernel module if not loaded
+ *   - Calls `rtnl_create_link`
+ *     - Calls `alloc_netdev_mqs` to allocate the `net_device`
+ *     - Notifies WG `device.c` via `setup` op
+ *     - Allocates various queues
+ *   - Notifies WG `device.c` via `newlink` op
+ *     - Calls `register_netdevice` to install new device
+ *   - Calls `rtnl_configure_link`
+ *     - Sets `rtnl_link_state` to `RTNL_LINK_INITIALIZED`
+ */
+struct net_device* stub_rtnl_newlink()
+{
+  // Initialize the kernel module as if just loaded.
+  init_module();
+
+  // Initialize `net_device` similar to `alloc_netdev_mqs`.
+  struct net_device *dev = stub_alloc_netdev_mqs();
+
+  // Notify WG via `newlink` op of the succesfully allocated link.
+  // Ops recorded as global by stubs in `rtnetlink.c`
+  wg_link_ops.newlink(/* src_net */ NULL, dev,
+                      /* tb */ NULL, /* data */ NULL, /* ext_act */ NULL);
+
+  return dev;
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,18 +124,9 @@ int main(int argc, char* argv[])
    *   - IFLA_IFNAME: wg0
    *   - IFLA_INFO_KIND: wireguard
    *
-   * Received by `rtnl_newlink` which:
-   *   - Loads the kernel module if not loaded
-   *   - Calls `rtnl_create_link`
-   *     - Calls `alloc_netdev_mqs` to allocate the `net_device`
-   *     - Notifies WG `device.c` via `setup` op
-   *     - Allocates various queues
-   *   - Notifies WG `device.c` via `newlink` op
-   *     - Calls `register_netdevice` to install new device
-   *   - Calls `rtnl_configure_link`
-   *     - Sets `rtnl_link_state` to `RTNL_LINK_INITIALIZED`
+   * Received by `rtnl_newlink`, similar to the stub above.
    */
-  init_module();
+  struct net_device* dev = stub_rtnl_newlink();
 
   /*
    * $ ip address add dev wg0 192.168.2.1 peer 192.168.2.2
@@ -77,30 +165,27 @@ int main(int argc, char* argv[])
   // assumptions later on to also test what happens if the kernel misbehaves.
 
   // Skip ahead and try forcing a socket!
-  // TODO: Remove this once `setup` is called, or make symbolic...?
-  memset(&wg0, 0, sizeof(wg0));
-
-  wg_socket_init(&wg0, 12345);
+  wg_socket_init(netdev_priv(dev), 12345);
 
   // Create a symbolic packet to receive
   struct sk_buff skb;
   klee_make_symbolic(&skb, sizeof(skb), "r_skb");
 
   // Initialize similar to `alloc_skb`
-  // `alloc_size` is the size of the buffer.
+  // `buffer_size` is the size of the buffer.
   // The kernel defines a max size of SKB_MAX_ALLOC = PAGE_SIZE << 2 = 16384.
   // The tunnel's MTU defaults to 1420. Tracing shows the full buffer size maxes
   // out around 2304. Let's be safe and offer a bit more room for now.
-  size_t alloc_size = 3072;
-  char *data = malloc(alloc_size);
-  klee_make_symbolic(data, alloc_size, "r_data");
+  size_t buffer_size = 3072;
+  char *data = malloc(buffer_size);
+  klee_make_symbolic(data, buffer_size, "r_data");
   // TODO: Adjust size for `skb_shared_info`?
 
   // Clear most `skb` metadata.
   // TODO: Relax some of these, such as `len`?
   memset(&skb, 0, offsetof(struct sk_buff, tail));
   // Records data size plus skb and skb shared structs.
-  skb.truesize = SKB_TRUESIZE(alloc_size);
+  skb.truesize = SKB_TRUESIZE(buffer_size);
 
   // Set some basics, such as pointers, to concrete values to save query time.
   // TODO: Relax some of these...?
@@ -108,7 +193,7 @@ int main(int argc, char* argv[])
   skb.head = data;
   skb.data = data;
   skb.tail = 0;
-  skb.end = skb.tail + alloc_size;
+  skb.end = skb.tail + buffer_size;
 
   // For now, init these headers like `alloc_skb`.
   skb.mac_header = (typeof(skb.mac_header))~0U;
@@ -116,12 +201,12 @@ int main(int argc, char* argv[])
 
   // Set symbolic head size like `skb_reserve`.
   const size_t head_size =
-    klee_range(0, alloc_size + 1, "r_head_size");
+    klee_range(0, buffer_size + 1, "r_head_size");
   skb.data += head_size;
   skb.len += head_size;
 
   // Set symbolic data size like `skb_put`.
-  const size_t data_size = klee_range(0, alloc_size + 1, "r_data_size");
+  const size_t data_size = klee_range(0, buffer_size + 1, "r_data_size");
   skb.tail += data_size;
   skb.len += data_size;
 
@@ -129,13 +214,13 @@ int main(int argc, char* argv[])
 
   // Set symbolic transport header (UDP).
   const size_t transport_header_offset =
-    klee_range(0, alloc_size + 1, "r_transport_header_offset");
+    klee_range(0, buffer_size + 1, "r_transport_header_offset");
   skb.transport_header = skb.data - skb.head;
   skb.transport_header += transport_header_offset;
 
   // Set symbolic network header (IP).
   const size_t network_header_offset =
-    klee_range(0, alloc_size + 1, "r_network_header_offset");
+    klee_range(0, buffer_size + 1, "r_network_header_offset");
   skb.network_header = skb.data - skb.head;
   skb.network_header += network_header_offset;
 
--- verification/stubs/linux/net/rtnetlink.c
+++ verification/stubs/linux/net/rtnetlink.c
@@ -1,7 +1,10 @@
 #include <net/rtnetlink.h>
 
+struct rtnl_link_ops wg_link_ops;
+
 int rtnl_link_register(struct rtnl_link_ops *ops)
 {
+  wg_link_ops = *ops;
   return 0;
 }
 
