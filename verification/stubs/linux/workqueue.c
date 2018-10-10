5b6472c62888ab1975985e325f33ffdbcaba6b3b
J. Ryan Stinnett <jryans@gmail.com>
2018-10-10 22:41:57 +0200

    Add stubs for work queue memory management

--- verification/stubs/linux/workqueue.c
+++ verification/stubs/linux/workqueue.c
@@ -8,6 +8,19 @@
 #include <linux/types.h>
 #include <asm/bitops.h>
 
+struct workqueue_struct * __alloc_workqueue_key(
+	const char *fmt, unsigned int flags, int max_active,
+	struct lock_class_key *key, const char *lock_name, ...)
+{
+	// We don't currently create a queue at all, but NULL would suggest an error,
+	// so for now any non-zero value will suffice.
+	return (struct workqueue_struct *)1;
+}
+
+void destroy_workqueue(struct workqueue_struct *wq)
+{
+}
+
 void __queue_work(int cpu, struct workqueue_struct *wq,
                   struct work_struct *work)
 {
