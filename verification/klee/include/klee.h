49cc31d6009f26a549bd6834b4321f24f2662443
J. Ryan Stinnett <jryans@gmail.com>
2018-10-12 01:05:34 +0200

    Allow KLEE intrinsics in stub headers

--- verification/klee/include/klee.h
+++ verification/klee/include/klee.h
@@ -1,21 +1,33 @@
 /*
  * A small wrapper around KLEE intrinsics so they can be left in non-KLEE builds
- * with no effect.
+ * with no effect. In addition, we copy definitions from the normal KLEE headers
+ * instead of including them to avoid type conflicts between kernel headers and
+ * the stdlib headers that klee.h includes.
  */
 
 #ifndef _KLEE_H
 #define _KLEE_H
 
 #ifdef KLEE_VERIFICATION
-// Define this to avoid redefintion errors via `inttypes.h` in `klee/klee.h`
-#define _INTTYPES_H
-#include <klee/klee.h>
+
+void klee_make_symbolic(void *addr, size_t nbytes, const char *name);
+int klee_range(int begin, int end, const char *name);
+#define klee_assert(expr)                                                      \
+	((expr) ? (void)(0) :                                                  \
+		  __assert_fail(#expr, __FILE__, __LINE__,                     \
+				__PRETTY_FUNCTION__))
+
 #else // KLEE_VERIFICATION
-void klee_make_symbolic(void *addr, size_t nbytes, const char *name) {}
-int klee_range(int begin, int end, const char *name) {
-  return begin;
+
+void klee_make_symbolic(void *addr, size_t nbytes, const char *name)
+{
+}
+int klee_range(int begin, int end, const char *name)
+{
+	return begin;
 }
 #define klee_assert(expr)
+
 #endif // KLEE_VERIFICATION
 
 #endif // _KLEE_H
