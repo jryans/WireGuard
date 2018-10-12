264d4fc3e4a8db24e269a81898b9623d6ee4a96e
J. Ryan Stinnett <jryans@gmail.com>
2018-10-12 17:13:04 +0200

    Simplify ptr_ring to a queue of size 1
    This may not work out in end, but it's a good starting point at least,
    so we can move on to more stubs.

--- verification/stubs/linux/include/linux/ptr_ring.h
+++ verification/stubs/linux/include/linux/ptr_ring.h
@@ -9,7 +9,50 @@
 
 #include <linux/types.h>
 
+#include <klee.h>
+
 struct ptr_ring {
+	void *item;
 };
+
+static inline int ptr_ring_init(struct ptr_ring *r, int size, gfp_t gfp)
+{
+	r->item = NULL;
+	return 0;
+}
+
+static inline void ptr_ring_cleanup(struct ptr_ring *r, void (*destroy)(void *))
+{
+	klee_assert(!destroy);
+}
+
+static inline bool __ptr_ring_empty(struct ptr_ring *r)
+{
+	return r->item == NULL;
+}
+
+static inline void *__ptr_ring_peek(struct ptr_ring *r)
+{
+	return r->item;
+}
+
+static inline void __ptr_ring_discard_one(struct ptr_ring *r)
+{
+	r->item = NULL;
+}
+
+static inline void *ptr_ring_consume_bh(struct ptr_ring *r)
+{
+	void *item = r->item;
+	r->item = NULL;
+	return item;
+}
+
+static inline int ptr_ring_produce_bh(struct ptr_ring *r, void *ptr)
+{
+	klee_assert(__ptr_ring_empty(r));
+	r->item = ptr;
+	return 0;
+}
 
 #endif // _LINUX_PTR_RING_H
