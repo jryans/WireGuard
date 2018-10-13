42e98eef2e24ec7dacc1206f64b461a16371cadc
J. Ryan Stinnett <jryans@gmail.com>
2018-10-13 23:40:45 +0200

    Add more stubs for network device init

--- verification/stubs/linux/net/dev.c
+++ verification/stubs/linux/net/dev.c
@@ -10,6 +10,13 @@ int unregister_netdevice_notifier(struct notifier_block *nb)
 	return 0;
 }
 
+int register_netdevice(struct net_device *dev)
+{
+	// TODO: Emit NETDEV_REGISTER to notifier registered above.
+	// This appears to be only a minor administrative detail.
+	return 0;
+}
+
 void synchronize_net()
 {
 }
